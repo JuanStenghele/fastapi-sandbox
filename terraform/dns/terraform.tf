2c0df9588fa7a599b48882d2513a2574071f3dd6
Juan Stenghele <juan.stenghele@sezzle.com>
2026-05-01 20:34:44 +0000

    [ADD] - Terraform GitHub Actions pipelines

--- terraform/dns/terraform.tf
+++ terraform/dns/terraform.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = "~> 1.3"
 
+  cloud {
+    organization = "fastapi-sandbox"
+
+    workspaces {
+      name = "dns"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
--- terraform/infrastructure/terraform.tf
+++ terraform/infrastructure/terraform.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = "~> 1.3"
 
+  cloud {
+    organization = "fastapi-sandbox"
+
+    workspaces {
+      name = "infrastructure"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
