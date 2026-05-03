724e0ca79e6b2f3551a7c184c814b459fcd658cf
Juan Stenghele <juan.stenghele@sezzle.com>
2026-05-03 18:38:21 +0000

    [UPDATE] - EKS version

--- terraform/infrastructure/eks.tf
+++ terraform/infrastructure/eks.tf
@@ -18,7 +18,7 @@ module "eks" {
   version = "20.8.5"
 
   cluster_name    = "${var.app_name}-eks"
-  cluster_version = "1.29"
+  cluster_version = "1.35"
 
   vpc_id                                   = module.vpc.vpc_id
   subnet_ids                               = module.vpc.private_subnets
