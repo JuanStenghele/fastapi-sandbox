16594762edc07732b357faafd63ce6362cfe5f74
Juan Stenghele <juan.stenghele@sezzle.com>
2026-05-31 18:56:25 +0000

    [UPDATE] - EKS Terraform module version

--- terraform/infrastructure/eks.tf
+++ terraform/infrastructure/eks.tf
@@ -15,7 +15,7 @@ module "irsa-ebs-csi" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.36.0"
+  version = "~> 21.0"
 
   cluster_name    = "${var.app_name}-eks"
   cluster_version = "1.35"
