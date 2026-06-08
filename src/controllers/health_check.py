3c42911dcdd930d6d698e4207402e7c766244c25
Juan Stenghele <juanstenghele@gmail.com>
2026-06-08 01:14:07 +0000

    [ADD] - Deep health check

--- src/controllers/health_check.py
+++ src/controllers/health_check.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from constants import Tags
-from objects.health_check import HealthCheckResponse
+from objects.health_check import HealthCheckResponse, DeepHealthCheckResponse
 from dal.health_check_dal import HealthCheckDAL
 from clients.storage_client import StorageClient
 from dependency_injector.wiring import inject, Provide
@@ -13,13 +13,20 @@ router = APIRouter()
 
 
 @router.get("/health-check", response_model = HealthCheckResponse, tags = [Tags.HEALTH_CHECK])
+def health_check():
+  return HealthCheckResponse(
+    api = "ok"
+  )
+
+
+@router.get("/health-check/deep", response_model = DeepHealthCheckResponse, tags = [Tags.HEALTH_CHECK])
 @inject
-def health_check(
+def deep_health_check(
   session: Session = Depends(get_session),
   health_check_dal: HealthCheckDAL = Depends(Provide[Container.health_check_dal]),
   s3_client: StorageClient = Depends(Provide[Container.s3_client])
 ):
-  return HealthCheckResponse(
+  return DeepHealthCheckResponse(
     api = "ok",
     postgres_database = "ok" if health_check_dal.health_check(session) else "error",
     storage = "ok" if s3_client.health_check() else "error"
--- src/objects/health_check.py
+++ src/objects/health_check.py
@@ -3,5 +3,9 @@ from objects.base import BaseObj
 
 class HealthCheckResponse(BaseObj):
   api: str
+
+
+class DeepHealthCheckResponse(BaseObj):
+  api: str
   postgres_database: str
   storage: str
