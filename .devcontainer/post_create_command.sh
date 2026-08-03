b3202d88ba772399ac550f84cbadb4cd101f1626
Juan Stenghele <juanstenghele@gmail.com>
2026-08-03 03:00:06 +0000

    [UPDATE] - Replace Python container for Python Dev container

--- .devcontainer/post_create_command.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Install all Python dependencies
-pip install --upgrade --requirement /workspaces/fastapi-sandbox/requirements.txt --requirement /workspaces/fastapi-sandbox/test/requirements.txt
